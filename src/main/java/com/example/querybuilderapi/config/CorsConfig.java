7daf053c48993690421770c3564d6dbad32e07e7
Omar Al-Mashhadani <omaral-mashhadanimac@Omars-MacBook-Air.local>
2026-03-06 09:43:51 +0200

    added missing import

--- src/main/java/com/example/querybuilderapi/config/CorsConfig.java
+++ src/main/java/com/example/querybuilderapi/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
