651cf76be08d5840f0db4ef2cbee274019172ec2
Omar Al-Mashhadani <omaral-mashhadanimac@Omars-MacBook-Air.local>
2026-03-09 19:49:00 +0200

    fixed Circular Dependency

--- src/main/java/com/example/querybuilderapi/config/SecurityConfig.java
+++ src/main/java/com/example/querybuilderapi/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -125,10 +124,5 @@ public class SecurityConfig {
 
             response.sendRedirect(targetUrl);
         };
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
